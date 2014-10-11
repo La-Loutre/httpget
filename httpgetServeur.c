daecd9256c6897c9f90add9234aca8244df730e8
Louis Douriez <ldouriez@gmail.com>
2014-10-11 12:41:57 +0200

    changement serveur (2 processus)

--- httpgetServeur.c
+++ httpgetServeur.c
@@ -10,9 +10,10 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <wait.h>
 #include <arpa/inet.h>
 #include <signal.h>
-
+#define TAILLE_BUFFER 100
 static void erreur_si(int test,char *chaineErreur)
 {
   if(test)
@@ -21,6 +22,21 @@ static void erreur_si(int test,char *chaineErreur)
       exit(1);
     }
   
+}
+static void lecture(int socket )
+{
+  char *buffer=malloc(sizeof(char)*TAILLE_BUFFER);
+  int sizeRead;
+  while((sizeRead=read(socket,buffer,sizeof(char)*TAILLE_BUFFER))>0){
+      write(1,buffer,sizeRead*sizeof(char));
+      if(buffer[sizeRead-2]=='\n'||buffer[sizeRead-1]=='\n')
+	break;
+  }
+  close(socket);
+  free(buffer);
+
+
+
 }
 static int attend(int fd1)
 {
@@ -43,12 +59,12 @@ int main(int argc,char **argv)
   int socketfd;
   erreur_si(socket1<0,"Descripteur socket KO");
   struct in_addr in;
-  erreur_si(inet_aton("192.168.0.29",&in)==0,"Inet_aton");
+  erreur_si(inet_aton("0.0.0.0",&in)==0,"Inet_aton");
   fprintf(stderr,"\n\nAdresse : %x\n",in.s_addr);
   struct sockaddr_in sockin={AF_INET,htons(80),in};
   int err=bind(socket1,(struct sockaddr*)&sockin,sizeof(sockin));
   erreur_si(err==-1,"bind");
-  int err2;
+
   listen(socket1,1);
   struct sockaddr_in sockin2;
   socklen_t taillesockin2;
@@ -60,12 +76,21 @@ int main(int argc,char **argv)
     if(attendResult==1){
       socketfd=accept(socket1,(struct sockaddr*)&sockin2,&taillesockin2);
       erreur_si(socketfd<0,"Socket2");
-      fprintf(stderr,"Connection établie avec %x\n",sockin2.sin_addr.s_addr);
+      fprintf(stderr,"\nConnection établie avec %x\n",sockin2.sin_addr.s_addr);
+      pid_t p=fork();
+      if(p==0){
+      lecture(socketfd);
+      return 0;
+      }
       
-      sendto(socketfd,message,sizeof(char)*strlen(message),0,(struct sockaddr*)&sockin2,taillesockin2);
+    
+      if(sendto(socketfd,message,sizeof(char)*strlen(message),0,(struct sockaddr*)&sockin2,taillesockin2)==-1)
+	fprintf(stderr,"Erreur lors de l'envoie\n");
+     
       close(socketfd);
+      wait(NULL);
       fprintf(stderr,"Connection terminé avec %x\n",sockin2.sin_addr.s_addr);
-      //      fprintf(stderr,"%d",errno);
+     
     }
     else if(attendResult==-1)
       {
